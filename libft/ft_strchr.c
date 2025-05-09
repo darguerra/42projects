57104179df66834be38fedeec306662b8a748fc4
Dario <darioguerragarcia.lr@gmail.com>
2025-05-09 12:02:52 +0200

    Change about ft_strchr

--- libft/ft_strchr.c
+++ libft/ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: darguerr <darguerr@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/08 17:16:42 by darguerr          #+#    #+#             */
-/*   Updated: 2025/05/09 09:41:56 by darguerr         ###   ########.fr       */
+/*   Updated: 2025/05/09 12:02:26 by darguerr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,29 +16,22 @@ char	*ft_strchr(const char *str, int search_str)
 {
 	char			*nex_str;
 	size_t			i;
-	size_t			j;
-	size_t			boolean;
+	size_t			position;
 
 	nex_str[ft_strlen(str)];
 	i = 0;
-	j = 0;
-	boolean = 0;
+	position = NULL;
 	while (str[i] != NULL)
 	{
-		if (str[i] == search_str)
-		{
-			boolean = 1;
-			while (str != NULL)
-			{
-				nex_str[j] = str[i];
-				j++;
-				i++;
-			}
-			nex_str[i] = '\0';
-		}
-		i++;
+		if (str[i] == search_str && position == NULL)
+			position = i;
 	}
-	if (boolean == 0)
+	if (position == NULL)
 		return (NULL);
+	i = 0;
+	while ( str[i] == NULL)
+	{
+		nex_str[i] = str[i + position];
+	}
 	return (nex_str);
 }
