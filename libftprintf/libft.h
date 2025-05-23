9876ef05c3907655e0d37e210388cfb0efbb522f
Dario <darioguerragarcia.lr@gmail.com>
2025-05-23 21:53:47 +0200

    Add initial implementation of ft_printf and corresponding header file

---
+++ libftprintf/libft.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: darguerr <darguerr@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/23 21:50:55 by darguerr          #+#    #+#             */
+/*   Updated: 2025/05/23 21:52:39 by darguerr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+
+int	ft_printf(char const *, ...);
+
+#endif
