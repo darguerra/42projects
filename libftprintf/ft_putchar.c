302509917077624bde041817b182954eaad27f58
Dario <darioguerragarcia.lr@gmail.com>
2025-05-30 13:56:45 +0200

    change return about putchar function

--- libftprintf/ft_putchar.c
+++ libftprintf/ft_putchar.c
@@ -6,16 +6,15 @@
 /*   By: darguerr <darguerr@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/30 13:01:43 by darguerr          #+#    #+#             */
-/*   Updated: 2025/05/30 13:09:08 by darguerr         ###   ########.fr       */
+/*   Updated: 2025/05/30 13:55:40 by darguerr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
 
-int	ft_putchar(char c)
+void	ft_putchar(char c)
 {
 	write(1, &c,1);
-	return (0);
 }
 /*int	main(void)
 {
