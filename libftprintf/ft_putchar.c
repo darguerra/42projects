2a1783cf3ac3fdc58f97fc0f555cd04f4e3aa593
Dario <darioguerragarcia.lr@gmail.com>
2025-05-30 13:09:30 +0200

    finished putchar function

--- libftprintf/ft_putchar.c
+++ libftprintf/ft_putchar.c
@@ -6,7 +6,7 @@
 /*   By: darguerr <darguerr@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/30 13:01:43 by darguerr          #+#    #+#             */
-/*   Updated: 2025/05/30 13:05:36 by darguerr         ###   ########.fr       */
+/*   Updated: 2025/05/30 13:09:08 by darguerr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,20 @@
 
 int	ft_putchar(char c)
 {
-
+	write(1, &c,1);
+	return (0);
 }
+/*int	main(void)
+{
+	ft_putchar('c');
+	ft_putchar('o');
+	ft_putchar('c');
+	ft_putchar('a');
+	ft_putchar('\n');
+	ft_putchar('c');
+	ft_putchar('o');
+	ft_putchar('l');
+	ft_putchar('a');
+
+	return (0);
+}*/
