3f60f57156aade8ee8e2c21f56384cdf9b97c495
Dario <darioguerragarcia.lr@gmail.com>
2025-06-22 17:46:11 +0200

    Add get_next_line implementation and supporting files

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,13 +6,40 @@
 /*   By: darguerr <darguerr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/17 16:57:27 by darguerr          #+#    #+#             */
-/*   Updated: 2025/06/17 17:11:19 by darguerr         ###   ########.fr       */
+/*   Updated: 2025/06/22 17:45:42 by darguerr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+/* Get next line returns a read line of a file descriptor if is successful
+but if would there are other behaviors or errors must return NULL.
+*/
 char *get_next_line(int fd)
 {
+    static t_list   *list = NULL;
+    char            *next_line;
     
+    result = read(fd, buffer, 15);
+    if (result < 0) // < 0 or <= 0?
+        return (NULL);
+}
+
+int main(void)
+{
+    int fd;
+    char    *line;
+
+    //open the file with the flag READ ONLY
+    //return the file descriptor
+    fd = open("/file.txt/", O_RDONLY); 
+
+//use the GNL function with fd parameter inside in a loop while
+    line = get_next_line(fd);
+    while (line != NULL)
+    {
+        line = get_next_line(fd);
+    }
+
+close(fd);
 }
--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,16 +6,29 @@
 /*   By: darguerr <darguerr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/17 17:00:50 by darguerr          #+#    #+#             */
-/*   Updated: 2025/06/17 17:11:16 by darguerr         ###   ########.fr       */
+/*   Updated: 2025/06/19 16:47:53 by darguerr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+# define BUFFER_SIZE 10
+# endif
+
 # include <unistd.h>
 # include <stdio.h>
 # include <stdlib.h>
 # include <stdarg.h>
+#include <fcntl.h>
+
+typedef struct s_list
+{
+    char            *str_buf;
+    struct s_list   *next;
+}   t_list;
+
 
 char *get_next_line(int fd);
 #endif
